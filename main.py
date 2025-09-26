1442cb2e618d82cdbdc2830159de713e327a5300
Jan Linsen <janlinsench@gmail.com>
2025-09-26 22:40:50 +0200

    Update Main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 temp_const = [-20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40]
-temp = st.slider("Temperatur des Reiseortes", temp_const)
+temp = st.select_slider("Temperatur des Reiseortes", temp_const)
