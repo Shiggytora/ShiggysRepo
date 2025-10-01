20170777cec2b92f2e5eab5ed396e82d568d0f3f
Jan Linsen <janlinsench@gmail.com>
2025-10-01 12:55:59 +0200

    Main

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ with col1:
     <style>
     """
     st.markdown(page_bg_img, unsafe_allow_html = True)
-    st.title("Here you see our title")
-    st.write("This is the background")
+    
 
 with col2:
+    st.title("Here you see our title")
+    st.write("This is the background")
     people = st.selectbox("How many people are going on a trip?", (1, 2, 3, 4, "5 or more"), index=None, placeholder="Select amount of people") 
     days = st.slider("How many days should it be?", 0, 30)
     budget = st.select_slider("What is the maximum Budget per person?", budget_werte)
